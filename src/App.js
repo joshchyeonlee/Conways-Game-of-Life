5d7e833d1eac1c96d1d2c7ed73915b2c62baa484
joshchyeonlee <63981671+joshchyeonlee@users.noreply.github.com>
2022-05-09 17:09:38 -0600

    Make grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import { CardActionArea, Typography } from "@mui/material";
+
+const rows = 10;
+const cols = 10;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [grid, setGrid] = useState(() => {
+    const gridArray = Array.from({ length: rows }, () =>
+      Array.from( { length: cols}, () => false)
+    );
+    console.log(gridArray);
+    return gridArray;
+  });
+
+  const card = (
+    <Grid item xs={1}>
+      <Card>
+        <CardActionArea
+          sx={{
+            bgcolor: '#373737',
+            width: 50,
+            height: 50,
+            border: 1,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </CardActionArea>  
+      </Card>
+    </Grid> 
+  )
+
+  return (
+    <Grid container columns={10}
+      sx={{
+        width: 500,
+        height: 500,
+      }}
+    >
+      {grid.map((rows, i) => 
+        grid.map((cols, j) =>
+          card
+        ))}
+    </Grid>
   );
 }
 
