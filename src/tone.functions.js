dc38fa9717d3bbf6b8da774e34bfe7d18228fd84
joshchyeonlee <63981671+joshchyeonlee@users.noreply.github.com>
2022-07-05 15:15:36 -0600

    Add dropdown for scale selection

--- src/tone.functions.js
+++ src/tone.functions.js
@@ -2,7 +2,30 @@ import * as Tone from 'tone';
 
 const noteArray = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
 
-export function playGrid(grid){
+export const scaleArray = [{
+    name: "Chromatic Scale",
+    notes: [0,1,2,3,4,5,6,7,8,9,10,11],
+}, {
+    name: "Major Scale",
+    notes: [0,2,4,5,7,9,11],
+}, {
+    name: "Natural Minor Scale",
+    notes: [0,2,3,5,7,8,10],
+}, {
+    name: "Harmonic Minor Scale",
+    notes: [0,2,3,5,7,8,11],
+}, {
+    name: "Melodic Minor Scale",
+    notes: [0,2,3,5,7,9,11],
+}, {
+    name: "Major Pentatonic Scale",
+    notes: [0,2,4,7,9],
+}, {
+    name: "Minor Pentatonic Scale",
+    notes: [0,3,5,7,10],
+}];
+
+export function playGrid(grid, scale){
     const synth = new Tone.PolySynth(Tone.Synth).toDestination();
     const now = Tone.now();
 
@@ -14,10 +37,11 @@ export function playGrid(grid){
 
     for(let col = grid.length -1 ; col >= 0; col--){
         for(let row = 0; row < grid[col].length; row++){
-            let note = noteArray[noteIndex] + `${octaveCounter}`;
+            let note = noteArray[scale[noteIndex]] + `${octaveCounter}`;
+            console.log(note);
             noteIndex++;
 
-            if(noteIndex >= noteArray.length) {
+            if(noteIndex >= scale.length) {
                 noteIndex = 0;
                 octaveCounter++;
             }
